dafb6887a06a322db1ee5d86028e32b00fc5c249
marsh-sb <enotaku5526@gmail.com>
2020-11-25 03:48:30 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a889378dee674f838c5726ed2a1db6af19ebf908b596535125b7d986c6e3c0286f69d0547e664dca2a7f5f4e72b4fef156ad343807190bc2f14522572b254b92'
